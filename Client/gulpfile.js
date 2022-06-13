e25f213e53048bacd57510226ae6e20b45fb23b1
unknown <mohsaleh2010m>
2022-06-13 23:15:54 +0200

    starter react project

---
+++ Client/gulpfile.js
@@ -0,0 +1,13 @@
+// top level function [ task - src - - dest - watch]
+const gulp=require('gulp');
+const gulpsass=require('gulp-sass');
+const sass=gulpsass(require('sass'))
+
+
+ 
+ 
+
+gulp.task('sass',async function(){
+
+    gulp.src('src/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+})
