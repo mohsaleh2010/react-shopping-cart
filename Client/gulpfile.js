6371aa7123274af2492bb76a9f1cb18c72bedb31
unknown <mohsaleh2010m>
2022-06-14 23:34:12 +0200

    Footerr-comp

--- Client/gulpfile.js
+++ Client/gulpfile.js
@@ -1,13 +1,21 @@
 // top level function [ task - src - - dest - watch]
-const gulp=require('gulp');
-const gulpsass=require('gulp-sass');
-const sass=gulpsass(require('sass'))
+const gulp = require('gulp');
+const gulpsass = require('gulp-sass');
+const sass = gulpsass(require('sass'))
 
 
- 
- 
 
-gulp.task('sass',async function(){
+
+
+gulp.task('watchME', async function () {
 
     gulp.src('src/Components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
 })
+
+// add watchMe in file package.json
+// gulp.task('watchME',async function(){
+//     gulp.watch('src/Components/**/*.scss',async function(){
+
+//         gulp.src('src/Components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+//     })
+// })
